e7179181343c8f0f8f7997034c1a71c706b6d2d7
Daniel Githiomi <danielgithiomi@gmail.com>
2023-04-10 17:55:14 +0400

    Set up view binding in gradle module file

--- app/src/main/java/githiomi/dhosiolux/globalweather/MainActivity.java
+++ app/src/main/java/githiomi/dhosiolux/globalweather/MainActivity.java
@@ -2,17 +2,33 @@ package githiomi.dhosiolux.globalweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import githiomi.dhosiolux.globalweather.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    // View binding
+    private ActivityMainBinding mainActivityBinding;
+
+    // Views
+    TextView date, town, weatherType, mainTemp, wind, humidity, temp;
+    ImageView weatherImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Binding set up
+        mainActivityBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        // Update set content view
+        setContentView(mainActivityBinding.getRoot());
 
-
+        date = mainActivityBinding.TVDate;
+        town = mainActivityBinding.TVTownName;
 
     }
 }
